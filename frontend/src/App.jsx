58e267dbf9a41b85746b8f69bf08b29ab0028cb0
rohit singh <rs9990271.edu@gmail.com>
2026-02-18 07:33:04 +0530

    protected routes fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Layout from "./pages/Layout";
 import Dashboard from "./pages/Dashboard";
 import Profile from "./pages/Profile";
+import ProtectedRoute from "./components/ProtectedRoutes";
 
 export default function App() {
   return (
@@ -13,7 +14,13 @@ export default function App() {
         <Route path="/signup" element={<Signup />} />
 
         {/* All protected pages share the Layout with navbar */}
-        <Route element={<Layout />}>
+        <Route
+          element={
+            <ProtectedRoute>
+              <Layout />
+            </ProtectedRoute>
+          }
+        >
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/profile" element={<Profile />} />
         </Route>
